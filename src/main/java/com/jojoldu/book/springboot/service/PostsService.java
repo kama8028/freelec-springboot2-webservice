27d0e784ce8bff23d3dbe6cde641a946e15bef36
한용선 <kama8028@naver.com>
2021-12-24 15:04:52 +0900

    4.5 게시글 수정, 삭제 화면 만들기(4장 완료) - P.161까지 완료

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +29,19 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    //readOnly = true는 트랜젝션 범위는 유지하되 조회 기능만 남겨두어 조회 속도가 개선되기 때문에 등록, 수정, 삭제가 없는 서비스 메소드에 적합
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        //.map(PostListResponseDto::new) = .map(posts -> new PostsListResponseDto(posts))
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         /**
          *  머스테치 스타터로 인해 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
          *  앞의 경로는 src/main/resources/templates
@@ -24,6 +33,14 @@ public class IndexController {
          *  뒤의 파일 확장자는 .mustache
          */
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
