ca344e9691822d473bfc07d56aa5606bb48ff74e
한용선 <kama8028@naver.com>
2022-01-16 15:44:53 +0900

    10.4. 무중단 배포하기  - health.sh, start.sh, stop,.sh, switch.sh

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //URL, HTTP 메소드별로 관리가 가능함
                 // "/"등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람 권한을 주었습니다.
                 // "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 했습니다.
-                .antMatchers("/","/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll().antMatchers("/api/vi/**").hasRole(Role.USER.name())
+                .antMatchers("/","/css/**", "/images/**", "/js/**", "/h2-console/**","/profile").permitAll().antMatchers("/api/vi/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated() //나머지 url은 모두 인증된 사용자 들이게만 허용함(로그인한 사용자만 가능)
                 .and().logout().logoutSuccessUrl("/") //로그아웃 성공시 / 주소로 이동합니다.
                 .and().oauth2Login() //oauth2 로그인 기능에 대한 여러 설정의 진입점 입니다.
